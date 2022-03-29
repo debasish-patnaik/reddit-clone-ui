aa903e1f1bb35caff5d78749ba86f7ae4af35efa
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2022-03-29 20:42:50 +0530

    feat: adds graphql-codegen feat: links registration page with the backend

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,23 +1,28 @@
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button,
-} from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useMutation } from 'urql';
 import { InputField } from '../components/InputField';
 import Wrapper from '../components/Wrapper';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
-const Register = (props) => {
+const Register = () => {
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
