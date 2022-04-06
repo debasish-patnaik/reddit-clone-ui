1b07e05e05992591cb10715cdd9692873768a36a
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2022-04-06 15:25:20 +0530

    feat: loads more posts upon clicking load more
    feat: updates the posts query to implement pagination
    
    feat: implements pagination in the createUrqlClient file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,14 @@ import Layout from '../components/Layout';
 import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import NextLink from 'next/link';
+import { useState } from 'react';
 
 const Index = () => {
-  const [{ data, fetching }] = usePostsQuery({ variables: { limit: 10 } });
+  const [variables, setVariables] = useState({
+    limit: 33,
+    cursor: null as null | string,
+  });
+  const [{ data, fetching }] = usePostsQuery({ variables });
 
   if (!fetching && !data) {
     return <div>Your query failed for some reason.</div>;
@@ -33,7 +38,7 @@ const Index = () => {
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data!.posts.map((p) => (
+          {data!.posts.posts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
@@ -41,9 +46,20 @@ const Index = () => {
           ))}
         </Stack>
       )}
-      {data ? (
+      {data && data.posts.hasMore ? (
         <Flex>
-          <Button isLoading={fetching} m={'auto'} my={8} bg={'tan'}>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching}
+            m={'auto'}
+            my={8}
+            bg={'tan'}
+          >
             load more
           </Button>
         </Flex>
