97e375cbd8a1c31c6006e6754ca26c0b4071caeb
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2022-05-12 17:18:28 +0530

    enhancement: adds favicon and title to the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }: any) {
@@ -9,6 +10,27 @@ function MyApp({ Component, pageProps }: any) {
           useSystemColorMode: true,
         }}
       >
+        <Head>
+          <title>LiReddit</title>
+          <link rel="shortcut icon" href="favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicon-16x16.png"
+          />
+        </Head>
         <Component {...pageProps} />
       </ColorModeProvider>
     </ChakraProvider>
